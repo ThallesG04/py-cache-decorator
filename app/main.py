3d82949ddc25c5a7cca2a8d6f4d2a911376dfeb0
Thalles Gabriel <thallesgabrieldasilvacosta@gmail.com>
2025-09-06 01:12:48 -0300

    agora vai

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     result_dict = {}
 
-    def wrapper(*args:object) -> object:
+    def wrapper(*args: object) -> object:
         if args in result_dict:
             print("Getting from cache")
             return result_dict[args]
